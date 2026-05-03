57c5a1c858f85112dcc941da0967ec4fb8d2e291
Faisal N <contact@faisaln.com>
2026-05-03 02:23:31 -0400

    Add main hws functionality

---
+++ client.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const { URL } = require('url');
+
+class HwsClient {
+  constructor(options = {
+    server: 'https://sub.domain.tld:port/path',
+    cert: './certs/chain.pem',
+    callback: ((response) => { console.log('←', response) }),
+    close: (() => { console.log('↓ stream closed') }),
+    error: ((error) => { console.error('↓', error) })
+  }) {
+    this.server = options.server || 'https://sub.domain.tld:port/path';
+    this.cert = fs.readFileSync(options.cert || './certs/chain.pem');
+    this.callback = options.callback || (response => console.log('←', response));
+    this.close = options.close || (() => console.log('↓ stream closed'));
+    this.error = options.error || (error => console.error('↓', error));
+    this.err = this.err.bind(this);
+    this.request = null;
+  };
+
+  err(error) {
+    if (error === 'ended') {
+      this.close();
+    } else {
+      this.error(error);
+    };
+  };
+
+  send(data) {
+    const url = new URL(this.server);
+    if (typeof data === 'object') data = JSON.stringify(data);
+    this.request = ((url.protocol === 'https:') ? https : http).request(
+      {
+        hostname: url.hostname,
+        port: url.port || 443,
+        path: url.pathname,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(data)
+        },
+        ca: this.cert
+      },
+      res => {
+        var response = '';
+        res.on('data', r => (response += r));
+        res.on('end', () => this.callback(response));
+      }
+    );
+    this.request.on('error', this.err);
+    this.request.write(data);
+    this.request.end();
+  };
+
+  stop() {
+    if (this.request) this.request.destroy('ended');
+  };
+};
+
+module.exports = HwsClient;
---
+++ server.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const { Router } = require('express');
+const { Transform } = require('stream');
+
+class HwsStream extends Transform {
+  _transform(chunk, _enc, callback) {
+    const size = chunk.length.toString(16);
+    this.push(size + '\r\n');
+    this.push(chunk);
+    this.push('\r\n');
+    callback();
+  };
+};
+
+/**
+ * Returns the HWS server router.
+ *
+ * @param {Object} [options]
+ * @param {number} [options.maxBody='10mb']
+ * @returns {Router}
+ */
+function hwsRouter(options = {}) {
+  const router = Router();
+
+  router.use(require('express').raw({
+    type: '*/*',
+    limit: options.maxBody || '10mb',
+  }));
+
+  const messageQueue = [];
+
+  router.get('/', (req, res) => {
+    res.set({
+      'Content-Type': 'application/octet-stream',
+      'Transfer-Encoding': 'chunked',
+      Connection: 'keep-alive',
+    });
+    const stream = new HwsStream();
+    stream.pipe(res);
+    const drain = () => {
+      while (messageQueue.length) {
+        const ok = stream.write(Buffer.from(messageQueue.shift()));
+        if (!ok) {
+          stream.once('drain', drain);
+          return;
+        };
+      };
+      setImmediate(drain);
+    };
+    drain();
+    req.on('close', () => stream.end());
+  });
+
+  router.post('/', (req, res) => {
+    const body = req.body.toString();
+    console.log('Received from client:', body);
+    messageQueue.push(body);
+    res.type('text').send('OK');
+  });
+
+  return router;
+};
+
+module.exports = hwsRouter;
